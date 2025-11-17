276f7c0839b4cb1eba9fea4dfacda4c733059839
illider8bread <202684742+illider8bread@users.noreply.github.com>
2025-11-17 14:10:03 -0700

    Added background animation to introduction page

---
+++ src/components/Background.jsx
@@ -0,0 +1,60 @@
+import { useEffect } from "react"
+import background from "../assets/background-square.png"
+import gsap from "gsap";
+
+
+function Background() {
+
+    function backgroundAnim() {
+        let tl = gsap.timeline()
+        tl.add("startSimultaneous", "0")
+            .to("#square1",
+                {
+                    rotation: 360,
+                    duration: 50,
+                    ease: "linear",
+                    repeat: "-1"
+                },
+                "startSimultaneous")
+            .fromTo("#square2",
+                {
+                    rotation: -15,
+                    scale: 1.1,
+                    opacity: 0.66
+                },
+                {
+                    rotation: 360,
+                    duration: 55,
+                    ease: "linear",
+                    repeat: "-1"
+                },
+                "startSimultaneous")
+            .fromTo("#square3",
+                {
+                    rotation: -30,
+                    scale: 1.2,
+                    opacity: 0.33,
+                },
+                {
+                    rotation: 360,
+                    duration: 60,
+                    ease: "linear",
+                    repeat: "-1"
+                },
+                "startSimultaneous")
+
+    }
+
+    useEffect(() => {
+        backgroundAnim()
+    }, [])
+
+    return (
+        <div id="background">
+            <img src={background} id="square1" className="background__square" />
+            <img src={background} id="square2" className="background__square" />
+            <img src={background} id="square3" className="background__square" />
+        </div>
+    )
+}
+export default Background
--- src/pages/Introduction.jsx
+++ src/pages/Introduction.jsx
@@ -1,41 +1,46 @@
-import { useState } from "react";
 import Analysis from "../components/Analysis";
-import background from "../assets/form-bkg.png"
 import Form from "../components/Form";
 import NavigationButton from "../components/NavigationButton";
+import { useEffect } from "react";
+import Background from "../components/Background";
 
-function Introduction({createUser, loading, created}) {
+function Introduction({ createUser, loading, created }) {
 
-    function renderContent(){
-        if(sessionStorage.getItem("usercreated")===true || created){
-            return(
+    
+
+    function renderContent() {
+        if (sessionStorage.getItem("usercreated") === true || created) {
+            return (
                 <div className="position__centered submitted">
-                    Thank You!<br/>
+                    Thank You!<br />
                     Proceed for the next step
                 </div>
             )
-        } else if(loading){
-            return(
+        } else if (loading) {
+            return (
                 <div className="position__centered processing">
                     Processing Submission ...
                 </div>
             )
-        } else{
-            return(
-                <Form createUser={createUser}/>
+        } else {
+            return (
+                <Form createUser={createUser} />
             )
         }
     }
-    return(
+
+    return (
+        <>
+        <Background/>
         <div className="container">
             <div className="row">
-            <Analysis />
-            <img src={background} className="form__background--img position__centered"/>
-            {renderContent()}
-            <NavigationButton text="back" navTo="/"/>
-            {created ? <NavigationButton text="proceed" navTo="/image"/> : <></>}
+                <Analysis />
+                {renderContent()}
+                <NavigationButton text="back" navTo="/" />
+                {created ? <NavigationButton text="proceed" navTo="/image" /> : <></>}
             </div>
         </div>
+        </>
     )
 }
 
