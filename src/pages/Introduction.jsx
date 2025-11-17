97f82f08b652d0a2d288bd13011ce1d9b0138a90
illider8bread <202684742+illider8bread@users.noreply.github.com>
2025-11-17 14:49:47 -0700

    attempting to create a processing page animation

--- src/pages/Introduction.jsx
+++ src/pages/Introduction.jsx
@@ -3,10 +3,24 @@ import Form from "../components/Form";
 import NavigationButton from "../components/NavigationButton";
 import { useEffect } from "react";
 import Background from "../components/Background";
+import {gsap} from "gsap";
 
 function Introduction({ createUser, loading, created }) {
 
-    
+
+    function processingAnimation() {
+        let tl = gsap.timeline({repeat:-1});
+        tl.to("#dot1", { y: -16, duration: 0.25 }) 
+            .to("#dot1", { y: 16, duration: 0.25 }) 
+            .to("#dot2", { y: -16, duration: 0.25 }) 
+            .to("#dot2", { y: 16, duration: 0.25 }) 
+            .to("#dot3", { y: -16, duration: 0.25 }) 
+            .to("#dot3", { y: 16, duration: 0.25 });
+    }
+
+    // useEffect(() => {
+    //     processingAnimation()
+    // }, [])
 
     function renderContent() {
         if (sessionStorage.getItem("usercreated") === true || created) {
@@ -19,7 +33,10 @@ function Introduction({ createUser, loading, created }) {
         } else if (loading) {
             return (
                 <div className="position__centered processing">
-                    Processing Submission ...
+                    Processing Submission <br />
+                    <span id="dot1" className="processing__dot">.</span>
+                    <span id="dot2" className="processing__dot">.</span>
+                    <span id="dot3" className="processing__dot">.</span>
                 </div>
             )
         } else {
@@ -31,15 +48,21 @@ function Introduction({ createUser, loading, created }) {
 
     return (
         <>
-        <Background/>
-        <div className="container">
-            <div className="row">
-                <Analysis />
-                {renderContent()}
-                <NavigationButton text="back" navTo="/" />
-                {created ? <NavigationButton text="proceed" navTo="/image" /> : <></>}
+            <Background />
+            <div className="container">
+                <div className="row">
+                    <Analysis />
+                    <div className="position__centered processing">
+                        Processing Submission <br />
+                        <span id="dot1" className="processing__dot">.</span>
+                        <span id="dot2" className="processing__dot">.</span>
+                        <span id="dot3" className="processing__dot">.</span>
+                    </div>
+                    {/* {renderContent()} */}
+                    <NavigationButton text="back" navTo="/" />
+                    {created ? <NavigationButton text="proceed" navTo="/image" /> : <></>}
+                </div>
             </div>
-        </div>
         </>
     )
 }
