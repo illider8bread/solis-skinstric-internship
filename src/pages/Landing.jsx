554a0178ffc5ffa7ce4d615f9b605b3e254ef60c
illider8bread <202684742+illider8bread@users.noreply.github.com>
2025-11-17 13:03:43 -0700

    fixing spacing and css for landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -23,16 +23,16 @@ function Landing() {
                 "-=1")
             .to("#triangle1",
                 {
-                    scale:1.4,
+                    scale: 1.4,
                     opacity: 0.5,
-                    duration:1
+                    duration: 1
                 },
                 "-=1")
             .to("#triangle2",
                 {
-                    scale:1.2,
-                    opacity:0.75,
-                    duration:1
+                    scale: 1.2,
+                    opacity: 0.75,
+                    duration: 1
                 },
                 "-=1")
 
@@ -42,11 +42,15 @@ function Landing() {
         gsap.to("#animated__btn, .title, #triangle1, #triangle2",
             {
                 x: "0%",
-                scale:1,
-                opacity:1,
+                scale: 1,
                 duration: 1
             })
-        
+        gsap.to("#triangle1, #triangle2",
+            {
+                opacity:0
+            },
+        "")
+
     };
 
     useEffect(() => {
@@ -58,78 +62,80 @@ function Landing() {
 
     return (
         <div className="container">
-            <div className="row">
-                <section className="landing">
-                    <div
-                        id="animated__btn"
-                        className="button__left"
+            <section className="landing">
+                <div
+                    id="animated__btn"
+                    className="button__left"
+                >
+                    <img
+                        src={triangle}
+                        className="button__frame"
+                        alt="a decorative triangle"
+                    />
+
+                    <button id="nav__left"
+                    className="navigation__button landing__button">
+                        <img
+                            src={btn}
+                            className="button__icon"
+                            alt="directional triangle inside of a rotated square, pointing left"
+                        />
+
+                        <p className="navigation__button--text">
+                            Discover A.I.
+                        </p>
+                    </button>
+                </div>
+                <div className="title">
+                    Sophisticated skincare
+                </div>
+                <div className="button__right">
+                    <img
+                        src={triangle}
+                        className="button__frame right"
+                        id="triangle1"
+                        alt="a decorative triangle"
+                        style={{opacity:0}}
+                    />
+                    <img
+                        src={triangle}
+                        className="button__frame right"
+                        id="triangle2"
+                        alt="a decorative triangle"
+                        style={{opacity:0}}
+                    />
+                    <img
+                        src={triangle}
+                        className="button__frame right"
+                        id="triangle3"
+                        alt="a decorative triangle"
+                    />
+
+
+                    <button
+                        id="nav__right"
+                        className="navigation__button landing__button"
+                        onMouseEnter={slideOff}
+                        onMouseLeave={slideOn}
+                        onClick={() => navigate("/introduction")}
                     >
+                        <p className="navigation__button--text">
+                            Take Test
+                        </p>
+
                         <img
-                            src={triangle}
-                            className="button__frame"
-                            alt="a decorative triangle"
+                            src={btn}
+                            className="button__icon right"
+                            alt="directional triangle inside of a rotated square, pointing right"
                         />
-
-                        <button className="navigation__button landing__button">
-                            <img
-                                src={btn}
-                                className="button__icon"
-                                alt="directional triangle inside of a rotated square, pointing left"
-                            />
-
-                            <p className="navigation__button--text">
-                                Discover A.I.
-                            </p>
-                        </button>
-                    </div>
-                    <div className="title">
-                        Sophisticated skincare
-                    </div>
-                    <div className="button__right">
-                        <img
-                            src={triangle}
-                            className="button__frame right"
-                            id="triangle1"
-                            alt="a decorative triangle"
-                        />
-                        <img
-                            src={triangle}
-                            className="button__frame right"
-                            id="triangle2"
-                            alt="a decorative triangle"
-                        />
-                        <img
-                            src={triangle}
-                            className="button__frame right"
-                            id="triangle3"
-                            alt="a decorative triangle"
-                        />
-
-
-                        <button
-                            className="navigation__button landing__button"
-                            onMouseEnter={slideOff}
-                            onMouseLeave={slideOn}
-                            onClick={() => navigate("/introduction")}
-                        >
-                            <p className="navigation__button--text">
-                                Take Test
-                            </p>
-
-                            <img
-                                src={btn}
-                                className="button__icon right"
-                                alt="directional triangle inside of a rotated square, pointing right"
-                            />
-                        </button>
-                    </div>
-                    <div className="disclaimer">
-                        Skinstric developed an A.I. that creates
-                        a highly-personalised routine tailored to
-                        what your skin needs.
-                    </div>
-                </section>
-            </div>
+                    </button>
+                </div>
+                <div className="disclaimer">
+                    Skinstric developed an A.I. that creates
+                    a highly-personalised routine tailored to
+                    what your skin needs.
+                </div>
+            </section>
         </div>
     );
 }
