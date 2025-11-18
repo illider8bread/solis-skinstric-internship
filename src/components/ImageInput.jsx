08f539213bc4cabcb2ea7fc93b85dcf734bcb7b2
illider8bread <202684742+illider8bread@users.noreply.github.com>
2025-11-18 13:38:42 -0700

    removed unnecessary comments

--- src/components/ImageInput.jsx
+++ src/components/ImageInput.jsx
@@ -1,4 +1,3 @@
-import background from "../assets/form-bkg.png";
 import pointer from "../assets/pointer.png"
 import Background from "./Background";
 
@@ -7,9 +6,6 @@ function ImageInput({ icon, text, arrowDirection, onClick }) {
     return (
         <div className="image__input--wrapper">
             <div className="image__input--background">
-                {/* <img src={background}
-                    className="input__background--image"
-                    alt="decorative background diamond, surrounding a button" /> */}
                     <Background width="35vw"/>
                 <img src={pointer} className={`pointer ${arrowDirection}`} />
                 <p className={`image__input--label para__${arrowDirection}`}> {text} </p>
